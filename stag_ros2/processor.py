53132516c4d7f6090a128354f82d9d2f24af8fed
James R. Heselden <primordia@live.com>
2024-09-11 16:15:12 +0100

    experiments setup

--- stag_ros2/processor.py
+++ stag_ros2/processor.py
@@ -53,11 +53,20 @@ class Processor(Node):
         self.image_size = None
 
 
-        self.use_high_capacity_marker_detection = False #rosparam
-        self.use_enhanced_contrast_detection = False #rosparam
         self.overlay_proximity = 30 #rosparam
 
-        self.only_process_image_on_tigger = True #rosparam
+        a=0
+
+        if a == 0:
+            self.use_high_capacity_marker_detection = True #rosparam
+            self.use_enhanced_contrast_detection = False #rosparam
+            self.hamming = 'HD21'
+        else:
+            self.use_high_capacity_marker_detection = False #rosparam
+            self.use_enhanced_contrast_detection = False #rosparam
+            self.hamming = 'HD15'
+
+        self.only_process_image_on_trigger = False #rosparam
 
         self.process_image_trigger = True
         t = 'trigger_processor'
@@ -178,30 +187,17 @@ class Processor(Node):
             mg = m['g'][0]
             mb = m['b'][0]
 
-            # Prime method: (deprecated)
-            ## Prime index
-            #rpi = mr*3+0
-            #gpi = mg*3+1
-            #bpi = mb*3+2
-            #
-            ## Calculate prime value
-            #rp = sympy.prime(rpi) if rpi != 0 else 1
-            #gp = sympy.prime(gpi)
-            #bp = sympy.prime(bpi)
-            #uuid = rp * gp * bp
-            #
-            ## Log Output
-            #s = f"UUID: SliceID({sr},{sg},{sb}), PrimeIndex({rpi},{gpi},{bpi}), Prime({rp},{gp},{bp}), UUID({uuid})"
+            file_lengths = {23:6, 21:12, 19:38, 17:157, 15:766, 13:2884, 11:22335}
 
             # Scaled Bits
-            sr = mr * (hamming[0]**0)
-            sg = mg * (hamming[1]**1)
-            sb = mb * (hamming[2]**2)
-            uuid = sr + sg + bg
+            sr = mr * (file_lengths[hamming[0]]**0)
+            sg = mg * (file_lengths[hamming[1]]**1)
+            sb = mb * (file_lengths[hamming[2]]**2)
+            uuid = sr + sg + sb
 
             # Log Output
             s = f"UUID: MarkerID({mr},{mg},{mb}), SliceID({sr},{sg},{sb}), UUID({uuid})"
-            if sr == sg == sg:
+            if sr == sg == sb:
                 self.get_logger().warn(s)
             else:
                 self.get_logger().error(s)
@@ -227,7 +223,7 @@ class Processor(Node):
 
     def image_cb(self, msg):
         # Only trigger processing when commanded
-        if self.obly_process_image_on_trigger:
+        if self.only_process_image_on_trigger:
             if not self.process_image_trigger:
                 return
             self.process_image_trigger = False
@@ -244,22 +240,27 @@ class Processor(Node):
         # TODO: add each filter as a rosparam toggle
 
         # detect markers
-        if self.use_high_capacity_marker_detection == False:
-            ic, ii, ir = self.merge_image_findings(image, hamming)
+        #if self.use_high_capacity_marker_detection == False:
+        #    ic, ii, ir = self.merge_image_findings(image, hamming)
 
         #ic, ii, ir = self.merge_image_findings(image, 11)
+        #ic, ii, ir = self.merge_image_findings(image, 13)
+        #ic, ii, ir = self.merge_image_findings(image, 15)
         #ic, ii, ir = self.merge_image_findings(image, 17)
+        ic, ii, ir = self.merge_image_findings(image, 19)
+        #ic, ii, ir = self.merge_image_findings(image, 21)
         #ic, ii, ir = self.merge_image_findings(image, 23)
 
         # test all three chanels (as bgr)
-        if self.use_high_capacity_marker_detection == True:
-            b, g, r = image[:, :, 0], image[:, :, 1], image[:, :, 2]
-            blue, green, red = cv2.merge([b, b, b]), cv2.merge([g, g, g]), cv2.merge([r, r, r])
-            data = { 'r':dict(), 'g':dict(), 'b':dict() }
-            data['r']['c'], data['r']['i'], data['r']['r'] = self.merge_image_findings(r, hamming, save=False)
-            data['g']['c'], data['g']['i'], data['g']['r'] = self.merge_image_findings(g, hamming, save=False)
-            data['b']['c'], data['b']['i'], data['b']['r'] = self.merge_image_findings(b, hamming, save=False)
-            self.merge_overlays(data, [hamming, hamming, hamming])
+        #hamming=19
+        #if self.use_high_capacity_marker_detection == True:
+        #    b, g, r = image[:, :, 0], image[:, :, 1], image[:, :, 2]
+        #    blue, green, red = cv2.merge([b, b, b]), cv2.merge([g, g, g]), cv2.merge([r, r, r])
+        #    data = { 'r':dict(), 'g':dict(), 'b':dict() }
+        #    data['r']['c'], data['r']['i'], data['r']['r'] = self.merge_image_findings(r, hamming, save=False)
+        #    data['g']['c'], data['g']['i'], data['g']['r'] = self.merge_image_findings(g, hamming, save=False)
+        #    data['b']['c'], data['b']['i'], data['b']['r'] = self.merge_image_findings(b, hamming, save=False)
+        #    self.merge_overlays(data, [hamming, hamming, hamming])
 
         # save output for local reference
         ids = self.ids
@@ -279,7 +280,7 @@ class Processor(Node):
         if self.label_color_image:
             # draw detected markers with ids
             stag.drawDetectedMarkers(image, corners, ids)
-            stag.drawDetectedMarkers(image, rejected_corners, border_color=(255, 0, 0))
+            #stag.drawDetectedMarkers(image, rejected_corners, border_color=(255, 0, 0))
 
             # save resulting image
             ros_image = self.bridge.cv2_to_imgmsg(image, encoding="passthrough")
