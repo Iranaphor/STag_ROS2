2edece0c0c5054d3bb8a249be9bd1e70f142c1b0
James R Heselden <primordia@live.com>
2024-09-19 08:49:49 +0100

    modified hc marker generation

--- stag_ros2/merge_high_capacity_markers.py
+++ stag_ros2/merge_high_capacity_markers.py
@@ -1,40 +1,36 @@
 import os
-import sympy
 from pprint import pprint
 from PIL import Image
 
-
 # File paths
-input_dir = "HD11/"
-output_dir = "HD11_HC/"
-max_marker_id = (22335**3)+100
+#HD = 11
+#input_dir = "HD11/"
+#output_dir = "HD11_HC/"
 
-input_dir = "HD13/"
-output_dir = "HD13_HC/"
-max_marker_id = (2884**3)+100
+#HD = 13
+#input_dir = "HD13/"
+#output_dir = "HD13_HC/"
 
+#HD = 15
 #input_dir = "HD15/"
 #output_dir = "HD15_HC/"
-#max_marker_id = (766**3)+100
 
+#HD = 17
 #input_dir = "HD17/"
 #output_dir = "HD17_HC/"
-#max_marker_id = (157**3)+100
 
+#HD = 19
 #input_dir = "HD19/"
 #output_dir = "HD19_HC/"
-#max_marker_id = (38**3)+100
 
+#HD = 21
 #input_dir = "HD21/"
 #output_dir = "HD21_HC/"
-#max_marker_id = (12**3)+100
 
-#input_dir = "HD23/"
-#output_dir = "HD23_HC/"
-#max_marker_id = (6**3)+100
+HD = 23
+input_dir = "/home/james/Documents/STag-Markers/high-capacity/HD23/"
+output_dir = "/home/james/Documents/STag-Markers/high-capacity/HD23_HC/"
 
-
-DONT_RENDER_OUTPUT_IMAGES=True
 
 
 if not os.path.exists(output_dir):
@@ -90,70 +86,23 @@ def list_files(directory):
     return files
 
 
-files = list_files(input_dir)
+files = sorted(list_files(input_dir))
 print(files)
 
-HD = dict()
-for f in files:
-    idx = int(f.split('.png')[0])
-    HD[idx] = {
-        'filepath':f"{input_dir}{f}",
-        'prime_red':sympy.prime((idx*3)+0) if idx != 0 else 1,
-        'prime_green':sympy.prime((idx*3)+1),
-        'prime_blue':sympy.prime((idx*3)+2)
-    }
-print(HD)
+hd_r, hd_g, hd_b = len(files)**0, len(files)**1, len(files)**2
 
+# Generate markers
+for b_i, b_f in enumerate(files):
+    for g_i, g_f in enumerate(files):
+        for r_i, r_f in enumerate(files):
+            #if not ((r_i == b_i) or (r_i == g_i) or (b_i == g_i)): continue
 
-def prime_factors_sympy(n):
-    # Using sympy.factorint() which returns a dictionary
-    # where the keys are the prime factors and the values are their respective powers.
-    factors_dict = sympy.factorint(n)
+            # Calculate marker id
+            uuid = (r_i*hd_r) + (g_i*hd_g) + (b_i*hd_b)
+            print(f'M{uuid} | Format(R{r_i}-G{g_i}-B{b_i}))')
 
-    # To list the factors in the form of base and exponent pairs
-    factors_expanded = []
-    for prime, exp in factors_dict.items():
-        factors_expanded.extend([prime] * exp)
+            # Combine channels
+            output_file = f"{output_dir}HC{HD}-ID{uuid}.png"
+            combine_rgb_channels(f"{input_dir}{r_f}", f"{input_dir}{g_f}", f"{input_dir}{b_f}", output_file)
 
-    return factors_expanded
-
-
-count = 0
-for m in range(1,max_marker_id+1):
-    # Identify prime factors
-    f = prime_factors_sympy(m)
-
-    if len(f) == 0:
-        f += [1,1,1]
-
-    if len(f) == 1:
-        f += [1,1]
-
-    if len(f) == 2:
-        f += [1]
-
-    if len(f) > 3:
-        print(f'M{m} | {len(f)} Factors, Too Many, Skipping')
-        count += 1
-        continue
-
-    # Identify files for the factors
-    if not all(i in HD for i in f):
-        missing = [i for i in f if i not in HD]
-        print(f'M{m} | Factors Missing: {missing}')
-        count += 1
-        continue
-
-    if DONT_RENDER_OUTPUT_IMAGES:
-        continue
-
-    print(f'M{m} | {len(f)} Real Factors: {f}')
-    r, g, b = HD[f[0]], HD[f[1]], HD[f[2]]
-
-    # Combine channels
-    uuid = r['prime_red'] * g['prime_green'] * b['prime_blue']
-    output_file = f"{output_dir}HC{m}-ID{uuid}.png"
-    combine_rgb_channels(r['filepath'], g['filepath'], b['filepath'], output_file)
-
-print(f'\nOf {max_marker_id} attempts, {count} skipped leaving {max_marker_id-count}\n')
-#pprint(HD)
+print('\nGenerated markers.\n')
